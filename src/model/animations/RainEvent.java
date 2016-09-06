128105e43d944233e1ba4558829fd0229a4c4282
julianmaster <julienm86230@hotmail.fr>
2016-09-06 02:04:39 +0200

    Test to implement new game architecture #3

---
+++ src/model/animations/RainEvent.java
@@ -0,0 +1,12 @@
+package model.animations;
+
+import pattern.Component;
+import pattern.ComponentEvent;
+
+public class RainEvent extends ComponentEvent {
+
+	public RainEvent() {
+		super(RainComponent.class);
+	}
+
+}
