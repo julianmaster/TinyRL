54752b0d7d6b4dd224809fcace1808a43339f8eb
Julien Maitre <julien.maitre@univ-lr.fr>
2016-10-04 18:03:39 +0200

    Create the 32 colors dawnbringer palette

--- src/ui/CustomColor.java
+++ src/ui/CustomColor.java
@@ -3,9 +3,7 @@ package ui;
 import java.awt.Color;
 
 public class CustomColor {
-	// Dawnbringer
-	// https://github.com/geoffb/dawnbringer-palettes
-	// http://hem.bredband.net/ricfha/pic/db32.gpl
+	// Dawnbringer 16 colors
 	public static Color BLACK = new Color(20, 12, 28);
 	public static Color BLUE = new Color(48, 52, 109);
 	public static Color GREEN = new Color(52, 101, 36);
@@ -22,8 +20,46 @@ public class CustomColor {
 	public static Color LMAGENTA = new Color(215, 123, 186);
 	public static Color YELLOW = new Color(219, 215, 93);
 	public static Color WHITE = new Color(223, 239, 215);
+	
+	// Dawnbringer 32 colors
+	// https://github.com/geoffb/dawnbringer-palettes
+	// http://hem.bredband.net/ricfha/pic/db32.gpl
+//	public static Color BLACK = new Color(0, 0, 0);
+//	public static Color VALHALLA = new Color(34, 32, 52);
+//	public static Color LOULOU = new Color(69, 40, 60);
+//	public static Color OILED_CEDAR = new Color(102, 57, 49);
+//	public static Color ROPE = new Color(143, 86, 59);
+//	public static Color TAHITI_GOLD = new Color(223, 113, 38);
+//	public static Color TWINE = new Color(217, 160, 102);
+//	public static Color PANCHO = new Color(238, 195, 154);
+//	public static Color GOLDEN_FIZZ = new Color(251, 242, 54);
+//	public static Color ATLANTIS = new Color(153, 229, 80);
+//	public static Color CHRISTI = new Color(106, 190, 48);
+//	public static Color ELF_GREEN = new Color(55, 148, 110);
+//	public static Color DELL = new Color(75, 105, 47);
+//	public static Color VERDIGRIS = new Color(82, 75, 36);
+//	public static Color OPAL = new Color(50, 60, 57);
+//	public static Color DEEP_KOAMARU = new Color(63, 63, 116);
+//	public static Color VENICE_BLUE = new Color(48, 96, 130);
+//	public static Color ROYAL_BLUE = new Color(91, 110, 225);
+//	public static Color CORNFLOWER = new Color(0, 0, 0);
+//	public static Color VIKING = new Color(0, 0, 0);
+//	public static Color LIGHT_STEEL_BLUE = new Color(0, 0, 0);
+//	public static Color WHITE = new Color(0, 0, 0);
+//	public static Color HEATHER = new Color(0, 0, 0);
+//	public static Color TOPAZ = new Color(0, 0, 0);
+//	public static Color DIM_GRAY = new Color(0, 0, 0);
+//	public static Color SMOKEY_ASH = new Color(0, 0, 0);
+//	public static Color CLAIRVOYANT = new Color(0, 0, 0);
+//	public static Color BROWN = new Color(0, 0, 0);
+//	public static Color MANDY = new Color(0, 0, 0);
+//	public static Color PLUM = new Color(0, 0, 0);
+//	public static Color RAIN_FOREST = new Color(0, 0, 0);
+//	public static Color STINGER = new Color(0, 0, 0);
+	
+	
 
-	// Reiv
+	// Reiv 16 colors
 	// public static Color BLACK = new Color(17,11,9);
 	// public static Color BLUE = new Color(46,73,94);
 	// public static Color GREEN = new Color(108,107,37);
